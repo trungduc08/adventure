ece04a9471132ae026a11207845eba5f2aa5f819
trungduc08 <nguyentrugduc248@gmail.com>
2019-05-14 09:33:40 +0700

    Create project Adventure

---
+++ Assets/PenguinAdventure/Script/MyTool.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEditor;
+public class MyTool{
+	[MenuItem("MyTools/Screen Shot #a")]
+	private static void GetScrShot()
+	{
+		// File path
+		string folderPath = "E:/screenshots/";
+		string fileName = "scr";
+		
+		// Create the folder beforehand if not exists
+		if(!System.IO.Directory.Exists(folderPath))
+			System.IO.Directory.CreateDirectory(folderPath);
+		int i = 0;
+		while (System.IO.File.Exists(folderPath + fileName + ".png")) {
+			fileName = "scr" + i;
+			i++;
+		}
+
+		// Capture and store the screenshot
+		UnityEngine.ScreenCapture.CaptureScreenshot(folderPath + fileName + ".png");
+	}
+}
---
+++ Assets/PenguinAdventure/Script/Player/Recognize.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using UnityEngine.UI;
+using UnityEngine.Windows.Speech;
+
+using System.Linq;
+
+public class Recognize : MonoBehaviour {
+
+	[Header("Key words")]
+	[Tooltip("Controller actions of player")]
+	public KeywordRecognizer keywordRecognizer;
+	public Dictionary<string, Action> actions = new Dictionary<string, Action>();
+
+	// Use this for initialization
+	void Start () {
+		actions.Add("down", () =>{
+			goCall();
+		});
+		keywordRecognizer = new KeywordRecognizer(actions.Keys.ToArray());
+		keywordRecognizer.OnPhraseRecognized += Recognizer_OnPhraseRecognized;
+		keywordRecognizer.Start();
+	}
+	
+	private void Recognizer_OnPhraseRecognized(PhraseRecognizedEventArgs args)
+    {
+        // word = args.text;
+		// Debug.Log("word: "+ args.text);
+		// actions[args.text].Invoke();
+		Action keywordAction;
+		if(actions.TryGetValue(args.text, out keywordAction)){
+			keywordAction.Invoke();
+		}
+    }
+
+	void goCall(){
+		Debug.Log("go call");
+	}
+}
